8a73b951438a9fc1dac7e952f48c8d191d613c3f
Prasanna Pendse <prasanna.pendse@gmail.com>
2020-08-14 16:31:20 +0530

    Got tests to pass

--- test/test_pyrfdata/test_request.py
+++ test/test_pyrfdata/test_request.py
@@ -5,12 +5,12 @@ import unittest
 from unittest.mock import MagicMock
 
 src_dir = os.path.realpath(
-    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../src/pyrfdata")))
+    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "../../src")))
 
 if src_dir not in sys.path:
     sys.path.insert(0, src_dir)
 
-import request
+from pyrfdata import request
 
 
 class TestRequest(unittest.TestCase):
